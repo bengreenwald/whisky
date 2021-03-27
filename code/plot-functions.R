803106e0e832c9b929617582f9192062923e55b3
bengreenwald <68518131+bengreenwald@users.noreply.github.com>
2021-03-27 11:01:25 -0400

    pushing files to repo

---
+++ code/plot-functions.R
@@ -0,0 +1,145 @@
+###############################
+#### CUSTOM PLOT FUNCTIONS ####
+###############################
+
+# Ridgeline Plot --------------
+
+ridgeline_plot <- function(var,
+                           group = type,
+                           log_trans = FALSE) {
+  
+  # create dynamic axis title
+  axis_title <- stringr::str_to_title(rlang::ensym(var))
+  
+  var <- rlang::enquo(var)
+  group <- rlang::enquo(group)
+  
+  # create the base plot
+  rlp <- scotch %>% 
+    ggplot(aes(x = !!var, 
+               y = !!group, 
+               fill = !!group,
+               height = ..density..)) + 
+    geom_density_ridges(stat = "density",
+                        alpha = 0.8,
+                        trim = TRUE) + 
+    scale_fill_manual(values = c(wky_yellow, 
+                                 wky_orange, 
+                                 wky_brown)) + 
+    scale_y_discrete(expand = c(0, 0),
+                     labels = c("Blended Malt\nScotch Whisky\n\n",
+                                "Blended Scotch\nWhisky\n\n",
+                                "Single Malt\nScotch\n\n")) + 
+    coord_cartesian(clip = "off") + 
+    theme_bg() +
+    theme(legend.position = "none",
+          plot.title.position = "plot")
+  
+  # plot if no log transformation included
+  if(isFALSE(log_trans)) {
+    
+    rlp <- rlp + 
+      scale_x_continuous(expand = c(0, 0)) + 
+      labs(x = glue::glue("Distribution of {axis_title}"),
+           y = "")
+    
+  # plot if variable of interest is log transformed   
+  } else {
+
+    rlp <- rlp + 
+      scale_x_continuous(expand = c(0, 0),
+                         trans = "log10",
+                         breaks = scales::trans_breaks("log10", function(x) 10^x),
+                         labels = scales::trans_format("log10", scales::math_format(10^.x))) + 
+      labs(x = glue::glue("Distribution of Log({axis_title})"),
+           y = "")
+  
+  }
+  
+  rlp
+
+}
+
+# Hex Plot --------------
+
+hex_plot <- function(x,
+                     y,
+                     log_trans = NULL) {
+  
+  # create dynamic axis titles
+  x_axis_title <- stringr::str_to_title(rlang::ensym(x))
+  y_axis_title <- stringr::str_to_title(rlang::ensym(y))
+    
+  x <- rlang::enquo(x)
+  y <- rlang::enquo(y)
+  
+  # create base plot
+  hxp <- scotch %>% 
+    ggplot(aes(x = !!x, y = !!y)) + 
+    geom_hex() + 
+    scale_fill_gradientn(name = "Releases",
+                         colours = rev(brewer.pal(8, "YlOrBr")),
+                         n.breaks = 6) + 
+    labs(x = x_axis_title,
+         y = y_axis_title) + 
+    theme_bg_dark() + 
+    theme(plot.title = element_text(margin = margin(b = 20)),
+          plot.title.position = "plot") + 
+    guides(fill = guide_colourbar(barwidth = 1, 
+                                  barheight = 12,
+                                  ticks = FALSE))
+  
+  # plot if no log transformation included
+  if(is.null(log_trans)) {
+    
+    hxp +
+      labs(x = x_axis_title,
+           y = y_axis_title)
+  
+  } else {
+    
+    # plot if x-axis variable is log transformed     
+    if(log_trans == rlang::ensym(x)) {
+      
+      hxp + 
+        scale_x_continuous(trans = "log10",
+                           breaks = scales::trans_breaks("log10", function(x) 10^x),
+                           labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+        labs(x = glue::glue("Log({x_axis_title})"),
+             y = y_axis_title)
+      
+    # plot if x-axis variable is log transformed       
+    } else {
+      
+      hxp + 
+        scale_y_continuous(trans = "log10",
+                           breaks = scales::trans_breaks("log10", function(x) 10^x),
+                           labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+        labs(x = x_axis_title,
+             y = glue::glue("Log({y_axis_title})"))
+      
+    }
+    
+  }
+  
+}
+
+# Q-Q Plot --------------
+
+draw_qq_plot <- function(var) {
+ 
+  var <- rlang::enquo(var)
+  
+  # filter the data
+  data <- scotch %>% 
+    filter(!is.na(!!var)) %>% 
+    select(!!var) %>% 
+    as_vector
+  
+  # create q-q plot
+  ggqqplot(data, 
+           add = "qqline",
+           color = wky_brown,
+           ggtheme = theme_bg())
+   
+}
---
+++ code/plot-theme-bg.R
@@ -0,0 +1,86 @@
+##############################
+#### CUSTOM GGPLOT THEMES ####
+##############################
+
+# theme_bg() -----------------
+
+theme_bg <- function () { 
+  theme_minimal(base_family = "Roboto") %+replace% 
+    theme(
+      # titles, caption, subtitle, legend
+      plot.title = element_text(face = "bold",
+                                size = 13,
+                                margin = margin(b = 5),
+                                hjust = 0.5),
+      plot.subtitle = element_text(face = "italic",
+                                   size = 10.5,
+                                   margin = margin(b = 12.5),
+                                   hjust = 0.5),
+      plot.caption = element_text(size=8,
+                                  hjust = 1,
+                                  margin = margin(t = 10),
+                                  colour = "gray70"),
+      legend.title = element_text(face = "bold"),
+      # margin
+      plot.margin = margin(15, 15, 15, 15, "pt"),
+      # panel & background
+      plot.background = element_rect(fill = "gray95",
+                                     colour = "gray95"),
+      panel.background = element_rect(fill = "gray95",
+                                      colour = "gray95"),
+      panel.grid.major = element_line(colour = "gray85"),
+      panel.grid.minor = element_line(colour = "gray91"),
+      # axis text, ticks, labels
+      axis.text = element_text(size = 9),
+      axis.title.x = element_text(face = "bold",
+                                  margin = margin(t = 10)),
+      axis.title.y = element_text(face = "bold",
+                                  margin = margin(r = 10),
+                                  angle = 90)
+    )
+}
+
+# theme_bg_dark() ------------
+
+theme_bg_dark <- function () { 
+  theme_minimal(base_family = "Roboto") %+replace% 
+    theme(
+      # titles, caption, subtitle, legend
+      plot.title = element_text(face = "bold",
+                                size = 13,
+                                margin = margin(b = 5),
+                                hjust = 0.5,
+                                colour = "gray90"),
+      plot.subtitle = element_text(face = "italic",
+                                   size = 10.5,
+                                   margin = margin(b = 12.5),
+                                   hjust = 0.5,
+                                   colour = "gray90"),
+      plot.caption = element_text(size=8,
+                                  hjust = 1,
+                                  margin = margin(t = 10),
+                                  colour = "gray50"),
+      legend.title = element_text(face = "bold",
+                                  colour = "gray90"),
+      legend.text = element_text(colour = "gray90"),
+      # margin
+      plot.margin = margin(15, 15, 15, 15, "pt"),
+      # panel & background
+      plot.background = element_rect(fill = "gray10",
+                                     colour = "gray10"),
+      panel.background = element_rect(fill = "gray10",
+                                      colour = "gray10"),
+      panel.grid.major = element_line(colour = "gray25"),
+      panel.grid.minor = element_line(colour = "gray15"),
+      # axis text, ticks, labels
+      axis.text = element_text(colour = "gray80",
+                               size = 9),
+      axis.title.x = element_text(face = "bold",
+                                  margin = margin(t = 10),
+                                  colour = "gray90"),
+      axis.title.y = element_text(face = "bold",
+                                  margin = margin(r = 10),
+                                  angle = 90,
+                                  colour = "gray90")
+    )
+}
